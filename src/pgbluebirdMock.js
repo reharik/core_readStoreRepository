f2fa8e5f2016a5a1ede10b8171124746c9a3a375
Raif Harik <reharik@gmail.com>
2015-08-23 21:18:19 -0500

    commit and push

--- src/pgbluebirdMock.js
+++ src/pgbluebirdMock.js
@@ -3,35 +3,20 @@
  */
 
 var Promise = require('bluebird');
-var mock = function() {
-    return {
-        getById: mock.getById,
-        save: mock.save,
-        query: mock.query,
-        checkIdempotency: mock.checkIdempotency,
-        recordEventProcessed: mock.recordEventProcessed
+module.exports = {
+    getById: function(id, table) {
+        return Promise.resolve('success');
+    },
+    save: function(table, document, id) {
+        return Promise.resolve('success');
+    },
+    query: function(script) {
+        return Promise.resolve('success');
+    },
+    checkIdempotency: function(originalPosition, eventHandlerName) {
+        return Promise.resolve({isIdempotent:true});
+    },
+    recordEventProcessed: function(originalPosition, eventHandlerName, isNewSteam) {
+        return Promise.resolve('success');
     }
 };
-
-mock.prototype.getById =function(id, table) {
-    return Promise.resolve('success');
-};
-
-mock.prototype.save =function(table, document, id) {
-    return Promise.resolve('success');
-};
-
-mock.prototype.query=function(script) {
-    return Promise.resolve('success');
-};
-
-mock.prototype.checkIdempotency=function(originalPosition, eventHandlerName) {
-    return Promise.resolve({isIdempotent:true});
-};
-
-mock.prototype.recordEventProcessed=function(originalPosition, eventHandlerName, isNewSteam) {
-    return Promise.resolve('success');
-};
-
-
-module.exports = mock;
