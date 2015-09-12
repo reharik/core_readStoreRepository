783c7aff20e2d774b151f57c0993f881964ad064
Raif Harik <reharik@gmail.com>
2015-09-12 17:09:22 -0500

    updating registry

--- registry.js
+++ registry.js
@@ -1,11 +1,11 @@
 /**
  * Created by parallels on 9/3/15.
  */
-var container = require('dagon');
+var dagon = require('dagon');
 
 module.exports = function(_options) {
     var options = _options || {};
-
+    var container = dagon(options);
     return new container(x=>
         x.pathToRoot(__dirname)
             .requireDirectoryRecursively('./src')
